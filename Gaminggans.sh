b3e0f4bbca490525682248e2225cee346d81eff9
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2018-12-22 08:04:15 +0700

    Create Gaminggans.sh

---
+++ Gaminggans.sh
@@ -0,0 +1,308 @@
+#!/system/xbin/bash
+# Bersihkan Layar
+#mau ngapain liat liat 😑
+#mau reqod 
+#ijin dulu ma yang punya
+#tolol100%
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+figlet "MR XiD"|lolcat
+
+echo "              [=======================[+]=====================]"|lolcat
+echo "              [  Author :  -------------MR_XiD--------------  ]"
+echo "              [===============================================]"|lolcat
+echo "              [  EMAIL  :    safri4687gmail.com@gmail.com     ]"
+echo "              [  TEAM   :    D35TR0Y SQUAD                    ]"
+echo "              [  WA     :    083851312460                     ]"
+echo "              [===============================================]"|lolcat
+echo "              [        GUNAKAN TOOLS INI DENGAN BENAR         ]"
+echo "              [=======================[+]=====================]"|lolcat
+echo "              <  D35TR0Y SQUAD | JATIM BLACKHAT.              >"|lolcat
+echo "              |  ELITE MUSLIM CYBER ARMY | PONOROGO CREW        |"|lolcat
+echo "              [===============================================]"|lolcat
+echo "              [=======================[+]=====================]"|lolcat
+echo "              [+]  Mr_XiD| MA | MR EXPLOR5 | CRUSHER.       [+]"|lolcat
+echo "              [===============================================]"|lolcat
+
+
+echo  "=============================================================>"|lolcat
+echo  " 01) Red Hawk"
+
+echo  "=============================================================>"|lolcat
+echo  " 02) D-Tect"
+
+echo  "=============================================================>"|lolcat
+echo  " 03) Top Up Ilegal ML"
+
+echo  "=============================================================>"|lolcat
+echo  " 04) Top Up Ilegal FF"
+
+echo  "=============================================================>"|lolcat
+echo  " 05) Webdav"
+
+echo  "=============================================================>"|lolcat
+echo  " 06) Metasploit"
+
+echo  "=============================================================>"|lolcat
+echo  " 07) Pulsa Gratis"
+
+echo  "=============================================================>"|lolcat
+echo  " 08) Ubuntu"
+
+echo  "=============================================================>"|lolcat
+echo  " 09) Youtube Dl"
+
+echo  "=============================================================>"|lolcat
+echo  " 10) viSQL "
+
+echo  "=============================================================>"|lolcat
+echo  " 11) multi-brute-webdav "
+
+echo  "=============================================================>"|lolcat
+echo  " 12) Bajinganv6 "
+
+echo  "=============================================================>"|lolcat
+echo  " 13) Tools RCT"
+
+echo  "=============================================================>"|lolcat
+echo  " 14)  cc fresh Amazon Dan Untung Carding"
+
+echo  "=============================================================>"|lolcat
+echo  " $red 15) Exit "
+echo  $white""
+read -p "[SANTRI GANS]> " bro;
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+echo  $red" Installing Red Hawk "
+sleep 1
+apt update && apt upgrade
+apt install php
+apt install git
+git clone 
+https://github.com/Tuhinshubhra/RED_HAWK
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 2 ] || [ $bro = 02 ]
+then
+clear
+echo  $red" Installing D-Tect "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install git
+apt-get install python2
+git clone 
+https://github.com/shawarkhanethicalhacker/D-TECT
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 3 ] || [ $bro = 03 ]
+then
+clear
+echo  $red" TOP UP ILEGAL ML "
+sleep 1
+echo  $white " Kode :5448adq35 | Kode 5644864y36 | Kode : 3158art6455 | Kode : 90426dka656 | Kode : 6456745hkw54 | Kode :2642x655843 "
+echo  $red" Masukin Kode Itu Ke Top Up Kalian ^_^"
+exit
+fi
+
+if [ $bro = 4 ] || [ $bro = 04 ]
+then
+clear
+echo  $red" TOP UP ILEGAL FF "
+sleep 1
+echo  $white"  Kode 5458453pr9 |Kode :260446i5m4 |Kode : 9705465d54 | Kode : 3455b623 | Kode : 84238y548 | Kode 862158t3944"
+echo  $red" Masukin Kode Itu Ke Top Up Kalian ^_^"
+exit
+fi
+
+if [ $bro = 5 ] || [ $bro = 05 ]
+then
+clear
+echo  $red" Installing Webdav "
+sleep 1
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna 
+requests
+apt install openssl curl
+pkg install libcurl
+mkdir webdav
+cd ~/webdav
+wget https://pastebin.com/raw/HnVyQPtR -O 
+webdav.py
+chmod 777 webdav.py
+cd ~/
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 06 ] || [ $bro = 6 ]
+then
+clear
+echo  $red" Installing Metasploit "
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install wget
+wget
+https://raw.githubusercontent.com/verluchie/termux-metasploit/master/install.sh
+chmod 777 install.sh
+sh install.sh
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 07 ] || [ $bro = 7 ]
+then
+clear
+echo  $red" PULSA GRATIS "
+sleep 1
+echo  $white" Inviet Kode :2bk340dk94 | Inviet Kode 154d955b9 |Inviet Kode : 9p54k644as5 | Inviet Kode 844ipps456 | Inviet Kode : 5qdk548239 | Inviet Kode :19ip9405"
+echo  $red" Masukin Kode Itu Ke Rendemm Pulsa Kalian ^_^"
+exit
+fi
+
+if [ $bro = 08 ] || [ $bro = 8 ]
+then
+clear
+echo  $red" Installing Ubuntu "
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install wget
+apt install proot
+git clone 
+https://github.com/Neo-Oli/termux-ubuntu.git
+cd ~/termux-ubuntu
+chmod +x ubuntu.sh
+sh ubuntu.sh
+echo " Fix network please wait "
+sleep 1
+echo "nameserver 8.8.8.8" 
+>/data/data/com.termux/files/home/termux-ubuntu/ubuntu-fs/etc/$
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 09 ] || [ $bro = 9 ]
+then
+clear
+echo  $red" Installing Youtube DL "
+sleep 1
+apt update && apt upgrade
+apt install python
+pip3 install mps_youtube
+pip3 install youtube_dl
+apt install mpv
+echo " Untuk menjalankannya ketik "mpsyt" 
+tanpa tanda petik "
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+echo  $red" Installing viSQL "
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install python2
+git clone 
+https://github.com/blackvkng/viSQL.git
+cd ~/viSQL
+chmod 777 viSQL.py
+cd ~/
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+echo  $red" Installing Multi-Brute-Webdav "
+apt update && apt upgrade
+pkg install toilet
+pkg install sh
+pkg install git
+git clone 
+https://github.com/INYOURS3RV3R/Multi-Brute-Webdav
+cd ~/Multi-Brute-Webdav
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ] 
+then
+clear
+echo  $red"installing bajinganv6"
+pkg install php
+pkg install figlet
+pkg install python2
+pip2 install requests
+pip2 install termcolor
+git clone 
+https://github.com/DarknessCyberTeam/BAJINGANv6.git
+cd ~/BAJINGANv6
+echo  $red" T E R I N S T A L L "
+exit 
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+echo  $red"installing tools RCT"
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/RabCT/rct
+cd ~/rct
+chmod +x rct
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+echo  $red" Tobat Bro Jangan Carding Trus Kasian Orang Tua Lu"
+sleep 1
+echo  $white" Name:Lita |Number Card : 2748074115574 Exp 2-2024 | Name : Dio |Number Card : 1264460632468 Exp -9-2021 | Name :Zaki |Number Card :541687450045 Exp : 4-2019 |Name : Panca |Number Card : 980412158457 Exp -8-2023 | Name : Rio | Number Card : 29045625807524 Exp -9 - 2020 | Name : Jessica | Number Card : 15507611507459 Exp -1 - 2024 | Name : Gilang | Number Card : 6878541334804 Exp -9-2026 |Name : Tia |Number Card :2087468412546 Exp -3-2021"
+echo  $red" Bila Ingin Lebih Lanjut Tunggu Peng Update an Selanjut nya"
+exit
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+echo " Thanks To Santri team official "
+sleep 1
+echo " Santri pasuruan"
+sleep 1
+echo " Bay-Bay "
+sleep 1
+echo " Para Hacker"
+sleep 1
+echo " Jangan Lupa Colii Yahh "
+sleep 1
+echo " Semoga Lu Besok Mati ea Budjank"
+sleep 1
+exit
+fi
